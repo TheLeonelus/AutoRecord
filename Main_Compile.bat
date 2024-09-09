1a7c640d65a80d96a48d966225088cfb789a839a
TheLeonelus <aloneTL@protonmail.ch>
2024-09-09 13:38:51 +0300

    use relative path for archieving

--- Main_Compile.bat
+++ Main_Compile.bat
@@ -2,4 +2,4 @@
 
 ".\Ahk2Exe.exe" /in ".\AutoRecord.ahk" /icon "icon.ico" /base ".\AutoHotkey64.exe"  /compress 0
 
-"C:\Program Files\7-Zip\7z.exe" a AutoRecord.zip %APPDATA%\AutoRecord\* -r -xr!.vscode -x!*.log x!*.old -x!*.bat -X!Ahk2exe.exe -X!AutoHotkey64.exe -X!*.zip -X!*.json
+"C:\Program Files\7-Zip\7z.exe" a AutoRecord.zip .\* -r -xr!.vscode -x!*.log x!*.old -x!*.bat -X!Ahk2exe.exe -X!AutoHotkey64.exe -X!*.zip -X!*.json
